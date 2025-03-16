6fa9e6eeec25da15b1d38cc18df8ba50a1d0aa5d
Kartik Khati <148654025+Kartikkhati@users.noreply.github.com>
2025-03-16 19:26:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-var crsr=document.querySelector("#cursor")
-var blur =document.querySelector("#cursor-blur")
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
 
+document.addEventListener("mousemove", function(dets) {
+    crsr.style.left = dets.clientX + "px";
+    crsr.style.top = dets.clientY + "px";
 
-document.addEventListener("mousemove",function(dets){
-    crsr.style.left=dets.x+"px"
-    crsr.style.top=dets.y+"px"
+    blur.style.left = dets.clientX -150+ "px";
+    blur.style.top = dets.clientY -150+ "px";
+});
 
-    blur.style.left=dets.x+"px"
-    blu.style.top=dets.y+"px"
-
+var h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale = 2
+        crsr.style.border = "0.5px solid #fff"
+        crsr.style.backgroundColor="transparent"
+    })
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale = 1
+        crsr.style.border = "0px solid #95c11e"
+        crsr.style.backgroundColor="#95c11e"
+    })
 })
-
 
 gsap.to("#nav", { // Upper top-down black effect
     backgroundColor: "rgba(0, 0, 0, 0.6)", 
@@ -19,7 +30,7 @@ gsap.to("#nav", { // Upper top-down black effect
     scrollTrigger: {
         trigger: "#nav",
         scroller: "body",
-        markers: true,
+        // markers: true,
         start: "top-10%", 
         end:"top -11%",
         scrub: 2
@@ -31,9 +42,61 @@ gsap.to("#main",{
     scrollTrigger:{
         trigger: "#main",
         scroller: "body",
-        markers: true,
+        // markers: true,
         start: "top -30%", 
         end: "top -100%",
         scrub: 2
     }
 });
+gsap.from("#about-us img,#about-us-in", {
+    y: 90,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#about-us",
+      scroller: "body",
+      // markers:true,
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 1,
+    },
+  });
+
+  gsap.from(".card", {
+      scale:0.8,
+      opacity: 0,
+      duration: 1,
+    //   stagger:0.1,
+      scrollTrigger: {
+      trigger: "#about-us", 
+      scroller: "body",
+      // markers:true,
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 1,
+    },
+  });
+  gsap.from("#colon1", {
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+      trigger: "#colon1",
+      scroller: "body",
+      // markers:true,
+      start: "top 55%",
+      end: "top 45%",
+      scrub: 4,
+    },
+  });
+  gsap.from("#colon2", {
+    y: 70,
+    x: 70,
+    scrollTrigger: {
+      trigger: "#colon1",
+      scroller: "body",
+      // markers:true,
+      start: "top 55%",
+      end: "top 45%",
+      scrub: 4,
+    },
+  });
